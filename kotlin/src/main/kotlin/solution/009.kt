6c4405f768567386c82050544d3fd4ac37b7c465
Taeyang Jin <sun@jupiterflow.com>
2021-11-29 21:26:50 +0900

    [009] Kotlin

---
+++ kotlin/src/main/kotlin/solution/009.kt
@@ -0,0 +1,20 @@
+package solution
+
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ */
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+
+        var variableX = x
+        var reversedX: Int = 0
+
+        while (0 < variableX) {
+            val digit = variableX % 10
+            reversedX = reversedX * 10 + digit
+            variableX /= 10
+        }
+
+        return (x == reversedX)
+    }
+}
